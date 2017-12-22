dc2945a6999cd2a590b29fb79a7f19fadafd66dd
philipp-schulz <34745613+philipp-schulz@users.noreply.github.com>
2017-12-22 15:13:56 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -17,15 +17,15 @@ class minecraft (
     ensure => file,
     content => 'eula=true'
   }
-  file {‘/etc/systemd/system/minecraft.service’:
+  file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    content => epp(‘minecraft/minecraft.service.epp’, {
+    content => epp('minecraft/minecraft.service.epp', {
       install_dir => $install_dir,
     }),
   }
   service { 'minecraft':
     ensure => running,
     enable => true,
-    require => [Package[‘java’],File[‘/opt/minecraft/eula.txt’],File[‘/etc/systemd/system/minecraft.service’]],
+    require => [Package['java'],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service']],
   }
 }
