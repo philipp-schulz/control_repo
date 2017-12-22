c112c726f6f024f68531cbb8be4bdb01c5aae53f
philipp-schulz <34745613+philipp-schulz@users.noreply.github.com>
2017-12-22 13:33:17 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDBL0qRVTV4alY4xye04enilPO3bUk2HcxwRhJwdFBePl/d/9dDDjEk9ZtU/qRGQiPV3Cvs67d0XzQDfMysBfSlULDu/rSp7xy4dqbZJvWZ15lGAdkCRnwgbhaKMcgHTw13DlAuN2YUweJ5r2YiSnsgJ0g0pgRIOKXDqiYqtQvaZJYdB06J/nvbN6XTd/oa53yp1tia/tgNG5H++M6slzrsnDWoC7BeHambrecyFWnrwy8r50nnHQrUU83qGOIxJv+ILVPyIPacYJr6sJsWH6wy2vg4H2+HfD//s82EVMkbkgbItpXgJ61GcZ24byrEykkHe33ki3/SgS5/9zFQl1ez',
 	}  
 }
